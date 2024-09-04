c2f5d46811f14dbd0d4048bbf2ae8ca542b08b0b
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-09-04 14:27:08 +0530

    update as per requirement

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\MumeneenModel;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateController extends Controller
 {
