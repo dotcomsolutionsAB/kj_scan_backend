493e0718e2ee18ff471c19b37cfdd06b0bda17d9
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-09-05 17:10:15 +0530

    update

--- app/Http/Controllers/CreateController.php
+++ app/Http/Controllers/CreateController.php
@@ -39,7 +39,9 @@ class CreateController extends Controller
                 // return response()->json([
                 //     'error' => 'Input Gender',
                 // ], 422);
-                echo "User not present";
+                // echo "User not present";
+                // Instead of stopping execution, store the error message
+                $response['errors'] = 'User not present';
 
                 $request->validate([
                     'gender' => 'required|string|in:male,female',
