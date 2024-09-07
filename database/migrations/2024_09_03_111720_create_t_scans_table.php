8a966c93008c5d18297607ab2d1bb52a566ea945
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-09-07 14:44:53 +0530

    update for `t_scans` migration file

--- database/migrations/2024_09_03_111720_create_t_scans_table.php
+++ database/migrations/2024_09_03_111720_create_t_scans_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->integer('its');
             $table->integer('entered_by');
             // $table->string('name');
-            $table->enum('gender', ['active', 'completed']);
+            $table->enum('gender', ['male', 'female']);
             $table->timestamps();
         });
     }
